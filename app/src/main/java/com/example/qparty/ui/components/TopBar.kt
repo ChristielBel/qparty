e6f7735c20337d8985815480cd78247e06141bce
ChristielBel <kristina04bel@gmail.com>
2025-08-16 19:41:57 +0300

    fix(topbar): fix topbar

--- app/src/main/java/com/example/qparty/ui/components/TopBar.kt
+++ app/src/main/java/com/example/qparty/ui/components/TopBar.kt
@@ -1,4 +1,4 @@
-package com.example.qparty
+package com.example.qparty.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
@@ -14,6 +14,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.example.qparty.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
