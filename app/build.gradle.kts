6ec37aaa1da9023e46e7db25ef82b5c18bec871c
ChristielBel <kristina04bel@gmail.com>
2025-08-25 12:21:27 +0300

    build: add libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.navigation.testing.android)
 
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
